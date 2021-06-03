78e079d88e120e3f3aa088dac2f72c42aa1e0e9c
xiongster94 <xiongster94@users.noreply.github.com>
2021-06-03 13:50:06 -0500

    mode dinnersupplies from App.jsx into DinnerSupplies.jsx

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer';
 import GuestList from '../GuestList/GuestList'
+import DinnerSupplies from '../DinnerSupplies/DinnerSupplies';
 
 function App() {
   let [guestList, setGuestList] = useState([]);
@@ -106,16 +107,7 @@ function App() {
         <button type="submit">Add Guest</button>
       </form>
       <GuestList list={guestList}/>
-      <h2>Dinner Supplies</h2>
-      <div>
-        Spoons: {guestList.length * 2}
-      </div>
-      <div>
-        Forks: {guestList.length * 2}
-      </div>
-      <div>
-        Knives: {guestList.length * 2}
-      </div>
+      <DinnerSupplies list={guestList}/>
       <Footer />
     </div>
   );
